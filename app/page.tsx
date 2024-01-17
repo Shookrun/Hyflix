6ac240f7129f0c02606cafeb5b0a301ea3d28213
Shookrun <sukranmamedov999@gmail.com>
2024-01-17 19:53:20 +0400

    Some Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hyflix</h1>
+      <h1 className="text-2xl text-green-500">Hyflix</h1>
     </div>
   )
 }
--- app/test.tsx
+++
@@ -1,9 +0,0 @@
-const MyPage = ()=>{
-    return(
-        <div>
-            <h1>Hello New Page</h1>
-        </div>
-    )
-}
-
-export default MyPage;
